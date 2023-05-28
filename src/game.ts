d2ceff48182ced9010662d544f020bee55f7ef6c
Rafik <bikmaevrafik@gmail.com>
2023-05-28 21:06:36 +0300

    Add ability to set infection probability

--- src/game.ts
+++ src/game.ts
@@ -73,6 +73,10 @@ export class Game {
         this.area.render();
     }
 
+    public setInfectionProbability(value: number): void {
+        this.areaStateManager.setInfectionProbability(value);
+    }
+
     public getSize(): number {
         return this.area.size;
     }
--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,20 @@ import { Game } from "./game";
 
     const timeoutInput = document.getElementById("timeout") as HTMLInputElement;
     timeoutInput.addEventListener("input", () => {
-        window["game"].timeoutInSec = parseFloat(timeoutInput.value);
+        window["game"].timeoutInSec = Number.parseFloat(timeoutInput.value);
     });
 
     const areaSizeInput = document.getElementById("area-size") as HTMLInputElement;
     areaSizeInput.addEventListener("input", () => {
-        window["game"].setSize(parseInt(areaSizeInput.value));
+        window["game"].setSize(Number.parseInt(areaSizeInput.value));
+    });
+
+    const infectionProbability = document.getElementById("infection-probability") as HTMLInputElement;
+    infectionProbability.addEventListener("input", () => {
+        window["game"].setInfectionProbability(Number.parseFloat(infectionProbability.value));
     });
 
     game.timeoutInSec = Number.parseFloat(timeoutInput.value);
     game.setSize(Number.parseInt(areaSizeInput.value));
+    game.setInfectionProbability(Number.parseFloat(infectionProbability.value));
 })(window);
